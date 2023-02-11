96689ea8ca6176852b9c8a6bea5ff9c01d11b38e
Milan Stojkovic <milan@svrljig.net>
2023-02-11 12:32:18 +0100

    script fix

--- build.sh
+++ build.sh
@@ -7,4 +7,4 @@ mvn -f websocket-connector/pom.xml clean &&
 mvn -f websocket-connector/pom.xml package -Dquarkus.package.type=uber-jar &&
 
 mvn -f web-api/pom.xml clean &&
-mvn -f web-api/pom.xml package -Dquarkus.package.type=uber-jar &&
+mvn -f web-api/pom.xml package -Dquarkus.package.type=uber-jar
